91663ea5a7eb58167516d5c07ccba724ad6dd1b6
arvinxx <arvinx@foxmail.com>
2023-08-18 00:59:01 +0800

    :fire: refactor: remove webCrawler

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,8 +1,6 @@
 import { PluginItem } from '../types/pluginItem';
 import searchEngine from './searchEngine';
-import webCrawler from './webCrawler';
 
 export const PluginsMap: Record<string, PluginItem> = {
   [searchEngine.name]: searchEngine,
-  [webCrawler.name]: webCrawler,
 };
