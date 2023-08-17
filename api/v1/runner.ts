ecf1c4ca0d319629a5dc7339495f7b0e3916040f
Arvin Xu <arvinx@foxmail.com>
2023-08-17 23:42:54 +0800

    :sparkles: feat: 初步剥离天气插件 (#1)

--- api/v1/runner.ts
+++ api/v1/runner.ts
@@ -1,9 +1,16 @@
+// TODO：后续替换为 sdk
+import { LobeChatPlugins } from '@lobehub/lobe-chat-plugins';
+
 import { PluginsMap } from '../../plugins';
 import { OpenAIPluginPayload } from '../../types/plugins';
 
 export const config = {
   runtime: 'edge',
 };
+
+const INDEX_PKG = `@lobehub/lobe-chat-plugins`;
+
+const INDEX_URL = `https://registry.npmmirror.com/${INDEX_PKG}/latest/files`;
 
 export default async (req: Request) => {
   if (req.method !== 'POST') return new Response('Method Not Allowed', { status: 405 });
@@ -12,6 +19,22 @@ export default async (req: Request) => {
 
   console.log(`检测到 functionCall: ${name}`);
 
+  console.timeLog('请求 index');
+  const res = await fetch(INDEX_URL);
+  const manifest: LobeChatPlugins = await res.json();
+  console.timeEnd('请求 index');
+
+  const item = manifest.plugins.find((i) => i.name === name);
+
+  // 先通过插件资产路径查询
+  if (item) {
+    const res = await fetch(item.runtime.endpoint, { body: args, method: 'post' });
+    const data = await res.text();
+    console.log(`[${name}]`, args, `result:`, data.slice(0, 3600));
+    return new Response(data);
+  }
+
+  // TODO：兼容性代码，全量完成迁移后移除
   const func = PluginsMap[name];
 
   if (func) {
@@ -22,6 +45,5 @@ export default async (req: Request) => {
 
     return new Response(JSON.stringify(result));
   }
-
   return;
 };
--- plugins/index.ts
+++ plugins/index.ts
@@ -1,10 +1,8 @@
 import { PluginItem } from '../types/pluginItem';
 import searchEngine from './searchEngine';
-import getWeather from './weather';
 import webCrawler from './webCrawler';
 
 export const PluginsMap: Record<string, PluginItem> = {
-  [getWeather.name]: getWeather,
   [searchEngine.name]: searchEngine,
   [webCrawler.name]: webCrawler,
 };
