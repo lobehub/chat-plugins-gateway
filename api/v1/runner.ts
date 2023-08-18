d0fc8888624193adbd299f01cbf172563afd63fc
arvinxx <arvinx@foxmail.com>
2023-08-18 21:21:48 +0800

    :fire: refactor: remove searchEngine

--- api/v1/runner.ts
+++ api/v1/runner.ts
@@ -1,7 +1,6 @@
 // TODO：后续替换为 sdk
 import { LobeChatPlugins } from '@lobehub/lobe-chat-plugins';
 
-import { PluginsMap } from '../../plugins';
 import { OpenAIPluginPayload } from '../../types/plugins';
 
 export const config = {
@@ -15,13 +14,13 @@ const INDEX_URL = `https://registry.npmmirror.com/${INDEX_PKG}/latest/files`;
 export default async (req: Request) => {
   if (req.method !== 'POST') return new Response('Method Not Allowed', { status: 405 });
 
+  const res = await fetch(INDEX_URL);
+  const manifest: LobeChatPlugins = await res.json();
+  console.log('manifest:', manifest);
+
   const { name, arguments: args } = (await req.json()) as OpenAIPluginPayload;
 
   console.log(`检测到 functionCall: ${name}`);
-
-  const res = await fetch(INDEX_URL);
-  const manifest: LobeChatPlugins = await res.json();
-  console.log(manifest);
 
   const item = manifest.plugins.find((i) => i.name === name);
 
@@ -33,16 +32,5 @@ export default async (req: Request) => {
     return new Response(data);
   }
 
-  // TODO：兼容性代码，全量完成迁移后移除
-  const func = PluginsMap[name];
-
-  if (func) {
-    const data = JSON.parse(args);
-    const result = await func.runner(data);
-
-    console.log(`[${name}]`, args, `result:`, JSON.stringify(result, null, 2).slice(0, 3600));
-
-    return new Response(JSON.stringify(result));
-  }
   return;
 };
--- plugins/index.ts
+++
@@ -1,6 +0,0 @@
-import { PluginItem } from '../types/pluginItem';
-import searchEngine from './searchEngine';
-
-export const PluginsMap: Record<string, PluginItem> = {
-  [searchEngine.name]: searchEngine,
-};
