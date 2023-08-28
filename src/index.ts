b209fba705c0dd3de97df4741fbf7264dc64b0a6
arvinxx <arvinx@foxmail.com>
2023-08-28 21:01:09 +0800

    ✨ feat: 支持入参包含 manifest

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,9 @@ export const createLobeChatPluginGateway = (pluginsIndexUrl: string) => {
       });
 
     // ==========  2. 校验请求入参基础格式 ========== //
-    const requestPayload = (await req.json()) as PluginRequestPayload;
+    const requestPayload = (await req.json()) as PluginRequestPayload & {
+      manifest?: LobeChatPluginManifest;
+    };
 
     const payloadParseResult = pluginRequestPayloadSchema.safeParse(requestPayload);
 
@@ -37,90 +39,92 @@ export const createLobeChatPluginGateway = (pluginsIndexUrl: string) => {
 
     const { identifier, arguments: args, indexUrl, apiName } = requestPayload;
 
+    let manifest: LobeChatPluginManifest | undefined = requestPayload.manifest;
     console.info(`[${identifier}] - ${apiName} `);
 
-    const marketIndexUrl = indexUrl ?? pluginsIndexUrl;
-    // ==========  3. 获取插件市场索引 ========== //
+    // 入参中如果没有 manifest，则从插件市场索引中获取
+    if (!manifest) {
+      const marketIndexUrl = indexUrl ?? pluginsIndexUrl;
+      // ==========  3. 获取插件市场索引 ========== //
 
-    let marketIndex: LobeChatPluginsMarketIndex | undefined;
-    try {
-      const indexRes = await fetch(marketIndexUrl);
-      marketIndex = await indexRes.json();
-    } catch (error) {
-      console.error(error);
-      marketIndex = undefined;
+      let marketIndex: LobeChatPluginsMarketIndex | undefined;
+      try {
+        const indexRes = await fetch(marketIndexUrl);
+        marketIndex = await indexRes.json();
+      } catch (error) {
+        console.error(error);
+        marketIndex = undefined;
+      }
+
+      // 插件市场索引不存在
+      if (!marketIndex)
+        return createErrorResponse(PluginErrorType.PluginMarketIndexNotFound, {
+          indexUrl: marketIndexUrl,
+          message: '[gateway] plugin market index not found',
+        });
+
+      // 插件市场索引解析失败
+      const indexParseResult = marketIndexSchema.safeParse(marketIndex);
+
+      if (!indexParseResult.success)
+        return createErrorResponse(PluginErrorType.PluginMarketIndexInvalid, {
+          error: indexParseResult.error,
+          indexUrl: marketIndexUrl,
+          marketIndex,
+          message: '[gateway] plugin market index is invalid',
+        });
+
+      console.info('marketIndex:', marketIndex);
+
+      // ==========  4. 校验插件 meta 完备性 ========== //
+
+      const pluginMeta = marketIndex.plugins.find((i) => i.identifier === identifier);
+
+      // 一个不规范的插件示例
+      // const pluginMeta = {
+      //   createAt: '2023-08-12',
+      //   homepage: 'https://github.com/lobehub/chat-plugin-real-time-weather',
+      //   manifest: 'https://registry.npmmirror.com/@lobehub/lobe-chat-plugins/latest/files',
+      //   meta: {
+      //     avatar: '☂️',
+      //     tags: ['weather', 'realtime'],
+      //   },
+      //   name: 'realtimeWeather',
+      //   schemaVersion: 'v1',
+      // };
+
+      // 校验插件是否存在
+      if (!pluginMeta)
+        return createErrorResponse(PluginErrorType.PluginMetaNotFound, {
+          identifier,
+          message: `[gateway] plugin '${identifier}' is not found，please check the plugin list in ${marketIndexUrl}, or create an issue to [lobe-chat-plugins](https://github.com/lobehub/lobe-chat-plugins/issues)`,
+        });
+
+      const metaParseResult = pluginMetaSchema.safeParse(pluginMeta);
+
+      if (!metaParseResult.success)
+        return createErrorResponse(PluginErrorType.PluginMetaInvalid, {
+          error: metaParseResult.error,
+          message: '[plugin] plugin meta is invalid',
+          pluginMeta,
+        });
+      // ==========  5. 校验插件 manifest 完备性 ========== //
+
+      // 获取插件的 manifest
+      try {
+        const pluginRes = await fetch(pluginMeta.manifest);
+        manifest = (await pluginRes.json()) as LobeChatPluginManifest;
+      } catch (error) {
+        console.error(error);
+        manifest = undefined;
+      }
+
+      if (!manifest)
+        return createErrorResponse(PluginErrorType.PluginManifestNotFound, {
+          manifestUrl: pluginMeta.manifest,
+          message: '[plugin] plugin manifest not found',
+        });
     }
-
-    // 插件市场索引不存在
-    if (!marketIndex)
-      return createErrorResponse(PluginErrorType.PluginMarketIndexNotFound, {
-        indexUrl: marketIndexUrl,
-        message: '[gateway] plugin market index not found',
-      });
-
-    // 插件市场索引解析失败
-    const indexParseResult = marketIndexSchema.safeParse(marketIndex);
-
-    if (!indexParseResult.success)
-      return createErrorResponse(PluginErrorType.PluginMarketIndexInvalid, {
-        error: indexParseResult.error,
-        indexUrl: marketIndexUrl,
-        marketIndex,
-        message: '[gateway] plugin market index is invalid',
-      });
-
-    console.info('marketIndex:', marketIndex);
-
-    // ==========  4. 校验插件 meta 完备性 ========== //
-
-    const pluginMeta = marketIndex.plugins.find((i) => i.identifier === identifier);
-
-    // 一个不规范的插件示例
-    // const pluginMeta = {
-    //   createAt: '2023-08-12',
-    //   homepage: 'https://github.com/lobehub/chat-plugin-real-time-weather',
-    //   manifest: 'https://registry.npmmirror.com/@lobehub/lobe-chat-plugins/latest/files',
-    //   meta: {
-    //     avatar: '☂️',
-    //     tags: ['weather', 'realtime'],
-    //   },
-    //   name: 'realtimeWeather',
-    //   schemaVersion: 'v1',
-    // };
-
-    // 校验插件是否存在
-    if (!pluginMeta)
-      return createErrorResponse(PluginErrorType.PluginMetaNotFound, {
-        identifier,
-        message: `[gateway] plugin '${identifier}' is not found，please check the plugin list in ${marketIndexUrl}, or create an issue to [lobe-chat-plugins](https://github.com/lobehub/lobe-chat-plugins/issues)`,
-      });
-
-    const metaParseResult = pluginMetaSchema.safeParse(pluginMeta);
-
-    if (!metaParseResult.success)
-      return createErrorResponse(PluginErrorType.PluginMetaInvalid, {
-        error: metaParseResult.error,
-        message: '[plugin] plugin meta is invalid',
-        pluginMeta,
-      });
-
-    // ==========  5. 校验插件 manifest 完备性 ========== //
-
-    // 获取插件的 manifest
-    let manifest: LobeChatPluginManifest | undefined;
-    try {
-      const pluginRes = await fetch(pluginMeta.manifest);
-      manifest = (await pluginRes.json()) as LobeChatPluginManifest;
-    } catch (error) {
-      console.error(error);
-      manifest = undefined;
-    }
-
-    if (!manifest)
-      return createErrorResponse(PluginErrorType.PluginManifestNotFound, {
-        manifestUrl: pluginMeta.manifest,
-        message: '[plugin] plugin manifest not found',
-      });
 
     const manifestParseResult = pluginManifestSchema.safeParse(manifest);
 
